7a09a6d7c32973375fd7af8afe800bbacdf02450
parigutierrez <parivash.gutierrez@gmail.com>
2017-06-14 16:54:39 -0500

    Se agrega funcionalidad de ver con modal

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ var api = {
 
 var $tasksList = $("#tasks-list");
 var $borrar = $(".borrar");
+var $info = $(".info");
 
 var cargarPagina = function () {
   cargarTareas();
   $(document).on("click", $borrar, pruebaBorrar);
   $("#add-form").submit(agregarTarea);
+  $(document).on("click", $info, datosModal);
 };
 
 var cargarTareas = function () {
@@ -23,6 +25,8 @@ var crearTarea = function (tarea) {
   var nombre = tarea.name;
   var estado = tarea.status[0];
   var id = tarea._id;
+  var date = tarea.create_at;
+
 
   var plantillaNueva = plantilla.replace("__nombre-tarea__", nombre).replace("__estado-tarea__",estado).replace("__id__",id);
   $tasksList.append(plantillaNueva);
@@ -44,9 +48,9 @@ var plantilla = '<tr data-clave="__id__">' +
                     '<td>__nombre-tarea__</td>' +
                     '<td>__estado-tarea__</td>' +
                    ' <td>'+
-                      '<span class="glyphicon glyphicon-eye-open"></span>' +
+                      '<span class="glyphicon glyphicon-eye-open info" data-toggle="modal" data-target="#modalInfo" ></span>' +
                       '<span class="glyphicon glyphicon-pencil"></span>' +
-                      '<span class="glyphicon glyphicon-trash" class="borrar"></span>' +
+                      '<span class="glyphicon glyphicon-trash borrar"></span>' +
                     '</td>'+
                   '</tr>';
 
@@ -68,10 +72,51 @@ function pruebaBorrar(e){
     }
 
   });
-
-
 }
 
+function datosModal(e){
+   var elemento = $(e.target);
+  var padre = elemento.parent().parent();
+ 
+  // console.log(id);
+  var id = padre.data('clave');
+  padre.remove();
+
+   var url_id = api.url + id;
+  $.getJSON(url_id,function(response){
+    console.log(response);
+
+    var nombre_api = response.name;
+    var _id = response._id;
+    var date = response.created_at;
+    var status = response.status[0];
+    console.log(nombre_api, _id, date, status);
+
+
+
+    imprimirInfo({
+      nombre: nombre_api,
+      clave: _id,
+      date: date,
+      status: status
+    })
+
+  })
+
+} 
+
+function imprimirInfo(objeto){
+  var $nombre = $("#nombre");
+  var $id = $("#id");
+  var $fecha = $("#fecha");
+  var $estado = $("#estado");
+
+  $nombre.text(objeto.nombre);
+  $id.text(objeto.clave);
+  $fecha.text(objeto.date);
+  $estado.text(objeto.status);
+ 
+}
 
 $(document).ready(cargarPagina);
 
